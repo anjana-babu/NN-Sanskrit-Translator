83a3ed898e63f047cc7a901f45320876c84f99ac
anjana <anjana@anjana.anjana>
2018-05-19 23:39:32 +0200

    Cleaned and prep data. Code for translation

--- data_prep.py
+++ data_prep.py
@@ -3,6 +3,9 @@ import re
 from pickle import dump
 from unicodedata import normalize
 from numpy import array
+from pickle import load
+from numpy.random import rand
+from numpy.random import shuffle
  
 # load doc into memory
 def load_doc(filename):
@@ -65,3 +68,27 @@ save_clean_data(clean_pairs, 'english-german.pkl')
 # spot check
 for i in range(100):
 	print('[%s] => [%s]' % (clean_pairs[i,0], clean_pairs[i,1]))
+
+# load a clean dataset
+def load_clean_sentences(filename):
+	return load(open(filename, 'rb'))
+ 
+# save a list of clean sentences to file
+def save_clean_data(sentences, filename):
+	dump(sentences, open(filename, 'wb'))
+	print('Saved: %s' % filename)
+ 
+# load dataset
+raw_dataset = load_clean_sentences('english-german.pkl')
+ 
+# reduce dataset size
+n_sentences = 10000
+dataset = raw_dataset[:n_sentences, :]
+# random shuffle
+shuffle(dataset)
+# split into train/test
+train, test = dataset[:9000], dataset[9000:]
+# save
+save_clean_data(dataset, 'english-german-both.pkl')
+save_clean_data(train, 'english-german-train.pkl')
+save_clean_data(test, 'english-german-test.pkl')
